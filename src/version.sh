832b4a8840824866e6d64404111215b1ecb881d7
Carsten Andrich <carsten.andrich@tu-ilmenau.de>
2024-06-03 23:28:03 +0200

    fix git origin and hash extraction

--- src/version.sh
+++ src/version.sh
@@ -1,9 +1,11 @@
 #!/bin/sh -eu
 
-if git --version >/dev/null 2>&1 && git -C "$1" status >/dev/null 2>&1 && git -C "$1" describe >/dev/null 2>&1 ; then
-    echo "extern const char git_origin[] = \"$(git -C "$1" config --get remote.origin.url)\";" >$2
-    echo "extern const char git_hash[] = \"$(git -C "$1" describe --always --long --broken --dirty)\";" >>$2
-else
-    echo "extern const char git_origin[] = \"UNKNOWN\";" >$2
-    echo "extern const char git_hash[] = \"UNKNOWN\";" >>$2
+if [ -e "$1/.git" ] && git --version >/dev/null 2>&1 ; then
+    git_origin=$(git -C "$1" config --get remote.origin.url)
+    git_hash=$(git -C "$1" describe --always --no-abbrev --broken --dirty)
 fi
+
+cat >$2 <<EOF
+extern const char git_origin[] = "${git_origin:-UNKNOWN}";
+extern const char git_hash[] = "${git_hash:-UNKNOWN}";
+EOF
